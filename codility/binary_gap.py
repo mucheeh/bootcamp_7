a0c796d6012b9d02cdae71fdbe6de591ed00e5d0
Hellen <smuche0841@gmail.com>
2016-04-28 10:47:26 +0300

    day 4

--- codility/binary_gap.py
+++ codility/binary_gap.py
@@ -1,22 +1,26 @@
-def solution(N):
-	max_ = 0
-	current = 0
+def solution(a):
+    max_ = 0
+    current = 0 
 
-	#skip the tailing zero(s)
-	while a > 0 and a%2 == 0:
-		a //= 2
+    #skip the tailing zero(s)
+    while a > 0 and a%2 == 0:
+        a //= 2
 
-	while a > 0:
-		rem = a%2
-		if rem == 0:
+    while a > 0:
+        rem = a%2
+        if rem == 0:
 
-			current += 1
-		else:
+            current += 1
+        else:
 
-			if current != 0:
-				max_ = max(current, max_)
-				current = 0
-		a //= 2
+            if current != 0:
+                max_ = max(current, max_)
+                current = 0
+        a //= 2
 
-	return max_
+    return max_
 
+print solution(15)
+print solution(150)
+print solution(20)
+print solution(529)
